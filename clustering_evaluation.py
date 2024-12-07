14e9ecf8a7837d2b692e87f07636e6bfed45ea68
Louisa Graef <louisagraef@gmail.com>
2024-12-07 22:35:25 +0100

    add validation test split, adjustments for refwug

--- clustering_evaluation.py
+++ clustering_evaluation.py
@@ -7,32 +7,21 @@ import os
 import glob
 from pathlib import Path
 from statistics import mean
+from sklearn.model_selection import train_test_split
+import unicodedata
 
 """
 Evaluate a clustering of one dataset with Adjusted Rand Index
 """
-def evaluate_clustering(dataset):
+def evaluate_clustering(dataset, words):
     ari_stats = {}                      # data to be saved 
     ari_stats['dataset'] = dataset
     dataset = "./data/" + dataset       # full path to dataset
 
-    words = sorted([d.name for d in Path(dataset + "/data").iterdir() if d.is_dir()])    # list of all words in the data directory 
     ri_values = []        # list of ri values of all words in the dataset 
     ari_values = []       # list of ari values of all words in the dataset 
 
     for word in words:
-        uses = dataset + "/data/" + word + "/uses.csv" 
-        graph = generate_graph(uses)                                    # generate graph 
-        clusters, cluster_stats = cluster_graph_cc(graph)               # cluster graph 
-        #print('Cluster stats: \n', cluster_stats)
-
-        # Get predicted labels 
-        node2cluster_inferred = {node:i for i, cluster in enumerate(clusters) for node in cluster}
-        node2cluster_inferred = {node:node2cluster_inferred[node] for node in graph.nodes}      # dictionary, maps node ids to cluster ids 
-        labels_pred = [node2cluster_inferred[node] for node in sorted(node2cluster_inferred.keys())]      # predicted labels (clusters) (sorted by node ids)
-        
-        #print('Predicted labels: \n', labels_pred)
-
         # Get Gold labels 
         gold_clusters = dataset + "/clusters/opt/" + word + ".csv"      # read in gold clusters 
         with open(gold_clusters, encoding='utf-8') as csvfile:
@@ -45,7 +34,17 @@ def evaluate_clustering(dataset):
         
         labels_true = [node2cluster_gold[node] for node in sorted(node2cluster_gold.keys())]        # gold labels (clusters) (sorted by node ids)
         
-        #print('Gold labels: \n', labels_true)
+
+        # Get predicted labels 
+        uses = dataset + "/data/" + word + "/uses.csv" 
+        uses = unicodedata.normalize('NFC', uses)           # normalize string 
+        graph = generate_graph(uses)                                    # generate graph 
+        clusters, cluster_stats = cluster_graph_cc(graph)               # cluster graph 
+
+        node2cluster_inferred = {node:i for i, cluster in enumerate(clusters) for node in cluster}
+        node2cluster_inferred = {node:node2cluster_inferred[node] for node in node2cluster_gold.keys()}   # dictionary, maps node ids to cluster ids 
+        labels_pred = [node2cluster_inferred[node] for node in sorted(node2cluster_inferred.keys())]      # predicted labels (clusters) (sorted by node ids)
+
 
         # Get Adjusted Rand Index 
         ri = rand_score(labels_true, labels_pred)
@@ -77,7 +76,7 @@ if __name__=="__main__":
 
     print("---")
     datasets = ["dwug_de", "discowug", "refwug", "dwug_en", "dwug_sv", "dwug_es", "chiwug", 
-                "nor_dia_change-main/subset1", "nor_dia_change-main/subset2"]
+                "nor_dia_change-main/subset1", "nor_dia_change-main/subset2"]           
     datasets_paper_versions = ["dwug_de", "dwug_en", "dwug_sv", "dwug_es", "chiwug", 
                 "nor_dia_change-main/subset1", "nor_dia_change-main/subset2"]
     
@@ -87,7 +86,10 @@ if __name__=="__main__":
         pass
     
     for dataset in datasets:
-        ari_stats, mean_ri, mean_ari  = evaluate_clustering(dataset)            # get clustering evaluation stats of one dataset 
+        words = sorted([f.stem for f in Path("./data/" + dataset + "/clusters/opt").iterdir() if f.is_file])    # list of all words in the clusters directory 
+        words_val, words_test = train_test_split(words, test_size=0.5, random_state=42)     # split words in validation and test sets, random and reproducible split
+        print(words_val)
+        ari_stats, mean_ri, mean_ari  = evaluate_clustering(dataset, words_val)            # get clustering evaluation stats of one dataset 
         print("\nDataset: ", dataset)
         print("Rand Index: ", mean_ri)
         print("Adjusted Rand Index: ", mean_ari)
